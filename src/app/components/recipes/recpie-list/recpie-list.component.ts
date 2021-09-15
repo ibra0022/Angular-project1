a10d58b2f1350e0869c1f907853838605b6d1510
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2021-09-15 09:19:51 +0300

    Update v1.1

--- src/app/components/recipes/recpie-list/recpie-list.component.ts
+++ src/app/components/recipes/recpie-list/recpie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recpie-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recpie-list.component.css']
 })
 export class RecpieListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'desc for recipe', 'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg')
+  ];
 
   constructor() { }
 
