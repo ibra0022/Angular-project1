f32cbf9e5c76f465438164babd7ecb815b317232
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2021-09-15 17:05:13 +0300

    Update v1.3

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
+  selcted: boolean = false;
+  theSelectedRecipe: Recipe;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelectFromList(recipe: Recipe) {
+    this.selcted = true;
+    this.theSelectedRecipe = recipe;
   }
-
 }
--- src/app/components/recipes/recpie-list/recpie-list.component.ts
+++ src/app/components/recipes/recpie-list/recpie-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recpie-list',
   templateUrl: './recpie-list.component.html',
-  styleUrls: ['./recpie-list.component.css']
+  styleUrls: ['./recpie-list.component.css'],
 })
 export class RecpieListComponent implements OnInit {
+  @Output() selectedRecFromList = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'desc for recipe', 'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'desc for recipe',
+      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'desc for recipe',
+      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectedRecipe(recipe: Recipe) {
+    this.selectedRecFromList.emit(recipe);
   }
-
 }
